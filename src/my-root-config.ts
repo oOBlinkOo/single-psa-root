75e070210474b8c3fa54aa6f2985c972136398d5
Miguel <miguel@processtempo.com>
2021-05-31 14:30:12 -0500

    adding

--- src/my-root-config.ts
+++ src/my-root-config.ts
@@ -11,10 +11,16 @@ import { registerApplication, start } from "single-spa";
 
 registerApplication({
   name: "app-root",
-  // app: () => System.import("app-root"),
   app: () => System.import("@my/holepage"),
   activeWhen: ["/"]
 });
+
+registerApplication({
+  name: "app-react",
+  app: () => System.import("@my/react"),
+  activeWhen: ["/"]
+});
+
 
 start({
   urlRerouteOnly: true,
